ae5daafdadf92456cae67d966d33d68a6660a128
DellMBP <balldel@gmail.com>
2018-02-22 23:34:58 +0700

    d

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def bot():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.type))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.type))
 
 if __name__ == "__main__":
    app.run()
