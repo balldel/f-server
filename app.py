b26960ccb8f656335cdf0f5bdb873653479a8409
ArsaNC <balldel@gmail.com>
2018-04-23 17:56:40 +0700

    h

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from linebot.models import (
 MessageEvent, TextMessage, TextSendMessage,
 )
 
+import requests 
+
 app = Flask(__name__)
 line_bot_api = LineBotApi('fSDjokoamI2lnlDZE8GJ2+PoZBn8DHsDba8zCtW57zR++3X+Iiy5jwtMQFB1oynrcHd3pU4g5S3IikMXzTmCkPueLieW/ilvst42POA6I6cyt/+z3u13OPxjof+Jq12l046ITxA2+sSMC95uRwEdHQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a3e92910d347b8dcda29a8bfaba8e3bc')
@@ -29,11 +31,23 @@ def bot():
     if 'scan>' in massage:
         countrycode = massage.split('>')[1]
         print(countrycode)
+        r = request.get('http://52.76.34.87:8000/skyscanner/api/country/?country='+countrycode)
+        print(r)
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    return 'OK'
+
+@app.route("/warakorn", methods=['GET'])
+def warakorn():
+
+    countrycode ='it'
+   
+    print(countrycode)
+    r = requests.get('http://127.0.0.1:8000/skyscanner/api/country/?country='+countrycode)
+    print(r.text)
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
