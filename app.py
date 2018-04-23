6539839ee6034fc989d614c672bdd79667798891
ArsaNC <balldel@gmail.com>
2018-04-23 16:33:59 +0700

    ss

--- app.py
+++ app.py
@@ -13,16 +13,15 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('fSDjokoamI2lnlDZE8GJ2+PoZBn8DHsDba8zCtW57zR++3X+Iiy5jwtMQFB1oynrcHd3pU4g5S3IikMXzTmCkPueLieW/ilvst42POA6I6cyt/+z3u13OPxjof+Jq12l046ITxA2+sSMC95uRwEdHQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a3e92910d347b8dcda29a8bfaba8e3bc')
 
-@app.route("/bot", methods=['GET'])
+@app.route("/bot", methods=['POST'])
 def bot():
-    # print('hello')
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
     data = request.json
     print(data)
-    # print("USERID :"+ data['events'][0]['source']['userId'])
+    print("USERID :"+ data['events'][0]['source']['userId'])
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
